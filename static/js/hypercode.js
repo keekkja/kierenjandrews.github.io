7179676eacb24fa2d3fb4700b5b13d15b9a826f2
Angus McMillan <nxtonic@gmail.com>
2015-11-21 01:33:19 +1100

    Header images now load randomly - optimised some more images - hypercode.js now activated

--- static/js/hypercode.js
+++ static/js/hypercode.js
@@ -5,10 +5,10 @@ window.hypercode = {
     for (i = 0; i < h.length; i++) {
       var index = Math.floor(Math.random() * (indexEnd - indexStart + 1)) + indexStart;
       var s = h[i];
-      s.style.backgroundImage = "url('./static/img/mask.png'), url('./static/img/header/"+ index +".png')";
+      s.style.backgroundImage = "url('./static/img/mask.png'), url('./static/img/header/"+ index +".jpg')";
       console.log(index);
     }
   }
 };
 
-// window.onload = window.hypercode.loadHeaderImage(0,4);
+window.onload = window.hypercode.loadHeaderImage(0,10);
